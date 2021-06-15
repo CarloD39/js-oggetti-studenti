84d140fea1f6e0856ef5bf274e5dd6a5e3e88a93
CarloD39 <carlodurand@hotmail.it>
2021-06-15 16:27:34 +0200

    part-2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var lonStudent = document.getElementById('lonely-student');
 
 var student = {
     name: 'Pino',
-    lastaName: 'Nopi',
+    lastName: 'Nopi',
     age: 122
 }
 
@@ -11,3 +11,22 @@ for(var k in student) { // k sta per key dell'oggetto student;
 }
 
 // ----------------
+
+var socialStudents = document.getElementById('students-group');
+
+var classroom = {
+    scholars : [
+        {nome: 'Carlo', cognome: 'Olrac',},
+        {nome: 'Luigi', cognome: 'Igiul'},
+        {nome: 'Luca', cognome: 'Acul'}
+    ]
+}
+
+for(var i = 0; i < classroom.scholars.length; i++){
+
+    for(var k in classroom.scholars[i]) {
+        socialStudents.innerHTML += '<li>' + (k,classroom.scholars[i] [k]) + '</li>';
+    }
+}
+
+// ------------------
